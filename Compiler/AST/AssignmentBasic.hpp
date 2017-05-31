5e69223f8edad9c952aaba3f6e5629dda698be25
EimantasMorkunas <eimamm@gmail.com>
2017-05-31 06:25:17 +0300

    fixes

--- Compiler/AST/AssignmentBasic.hpp
+++ Compiler/AST/AssignmentBasic.hpp
@@ -18,24 +18,27 @@ public:
 	Expression* exp;
     virtual ~AssignmentBasic() {}
     virtual string genCode(CodeGenContext* context) {
-		string type = exp->first->getType();
-		string typeName;
-		if (type == "INTEGER") {
-			typeName = "int";
+		if (context->variableTypes->find(variable->name) == context->variableTypes->end()) {
+			string type = exp->first->getType();
+			string typeName;
+			if (type == "INTEGER") {
+				typeName = "int";
+			}
+			if (type == "FLOAT") {
+				typeName = "double";
+			}
+			if (type == "CHAR") {
+				typeName = "char";
+			}
+			if (type == "STRING") {
+				typeName = "string";
+			}
+			if (type == "VAR") {
+				typeName = context->variableTypes->at((((Variable*)exp->first)->name));
+			}
+			context->variableTypes->insert(pair<string, string>(variable->name, typeName));
+			return typeName + " " + variable->genCode(context) + " = " + exp->genCode(context);
 		}
-		if (type == "FLOAT") {
-			typeName = "double";
-		}
-		if (type == "CHAR") {
-			typeName = "char";
-		}
-		if (type == "STRING") {
-			typeName = "string";
-		}
-		if (type == "VAR") {
-			typeName = context->variableTypes->at((((Variable*)exp->first)->name));
-		}
-		context->variableTypes->insert(pair<string, string>(variable->name, typeName));
-		return typeName + " " + variable->genCode(context) + " = " + exp->genCode(context);
+		return variable->genCode(context) + " = " + exp->genCode(context);
 	}
 };
