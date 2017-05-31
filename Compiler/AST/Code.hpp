b312e9af6746f98e620d5194ea40d0063bc8a957
EimantasMorkunas <eimamm@gmail.com>
2017-05-31 07:05:35 +0300

    fixes

--- Compiler/AST/Code.hpp
+++ Compiler/AST/Code.hpp
@@ -16,6 +16,11 @@ public:
 		string result = "";
 		for(std::vector<Function*>::iterator it = FunctionList.begin(); it != FunctionList.end(); ++it) {
 			context->variableTypes = new map<string, string>();
+			for (vector<Parameter*>::iterator it2 = (*it)->params->ParameterList.begin(); it2 != (*it)->params->ParameterList.end(); ++it2) {
+				string* a = new string (*((*it2)->type->name));
+				string* b = new string ((*it2)->name->name);
+				context->variableTypes->insert(pair<string, string>(*b, *a));
+			}
 			result += (*it)->genCode(context);
 			delete context->variableTypes;
 		}
